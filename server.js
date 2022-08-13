2311cf4d77b03d9572e137ff852471ff4ff89e8b
avishkakavindu <avishkakavindud@gmail.com>
2022-08-13 16:23:38 +0530

    Socket.io

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const cors = require("cors");
 const PORT = 8070;
 
 var messages = [
@@ -10,6 +13,7 @@ var messages = [
 
 app.use(express.static(__dirname));
 app.use(bodyParser.json());
+app.use(cors());
 // setting up bosy parser to support url encoded requests 
 app.use(bodyParser.urlencoded({extended: false}));
 
@@ -21,9 +25,16 @@ app.get('/messages', (req, res) => {
 // create message
 app.post('/messages', (req, res) => {
     messages.push(req.body);
+
+    io.emit('message', req.body)
+
     res.sendStatus(200);
 });
 
-const server = app.listen(PORT, () => {
+io.on('connection', (socket) => {
+    console.log('User connected!');
+});
+
+const server = http.listen(PORT, () => {
     console.log(`Server is listening on port ${server.address().port}`)
 });
