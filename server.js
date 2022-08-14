9ff051b22e1c6d1795183b8b2977bb61a3df7dae
avishkakavindu <avishkakavindud@gmail.com>
2022-08-14 19:10:00 +0530

    DB connections

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
+require("dotenv").config();
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const cors = require("cors");
+const mongoose = require('mongoose');
+
 const PORT = 8070;
 
 var messages = [
@@ -16,6 +19,11 @@ app.use(bodyParser.json());
 app.use(cors());
 // setting up bosy parser to support url encoded requests 
 app.use(bodyParser.urlencoded({extended: false}));
+
+
+const DB_PASSWORD = process.env.PASSWORD;
+
+const dbUrl = `mongodb+srv://user:${DB_PASSWORD}@cluster0.qyekysk.mongodb.net/?retryWrites=true&w=majority`;
 
 // get messages
 app.get('/messages', (req, res) => {
@@ -33,6 +41,10 @@ app.post('/messages', (req, res) => {
 
 io.on('connection', (socket) => {
     console.log('User connected!');
+});
+
+mongoose.connect(dbUrl, (err) => {
+    console.log('Mongo DB connection', err);
 });
 
 const server = http.listen(PORT, () => {
